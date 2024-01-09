4440e5c0304d69c87b056b24f00b35445a8fd253
王镇涛 <552194820@qq.com>
2024-01-09 20:52:59 +0800

    fdsfds

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 sudo service tomcat stop
-#sudo rm -rf /mnt/tomcat/webapps/web*
+sudo rm -rf /mnt/tomcat/webapps/web
 sudo chmod o+w /mnt/tomcat/*
 #sudo cp target/web.war /mnt/tomcat/webapps
 sudo chmod o+w /mnt/bizweb/log/bizweb.log
 sudo service tomcat start
 #tail -f /mnt/bizweb/log/bizweb.log
+
+
+sudo service tomcat stop
+sudo rm -rf /mnt/tomcat/webapps/web
+sudo cp target/web.war /mnt/tomcat/webapps
+sudo chmod o+w /mnt/tomcat/*
+sudo chmod o+w /mnt/bizweb/log/bizweb.log
+sudo service tomcat start
